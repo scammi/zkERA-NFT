13ddde1acdb3117602c2c7d354addca4c1728249
scammi <scammi@gmail.com>
2023-05-24 11:30:18 -0300

    MangoNFT deployment working

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -12,11 +12,11 @@ export default async function (hre: HardhatRuntimeEnvironment) {
 
   // Create deployer object and load the artifact of the contract you want to deploy.
   const deployer = new Deployer(hre, wallet);
-  const artifact = await deployer.loadArtifact("Greeter");
+  const artifact = await deployer.loadArtifact("Mango");
 
   // Estimate contract deployment fee
-  const greeting = "Hi there!";
-  const deploymentFee = await deployer.estimateDeployFee(artifact, [greeting]);
+  // const greeting = "Hi there!";
+  const deploymentFee = await deployer.estimateDeployFee(artifact, []);
 
   // OPTIONAL: Deposit funds to L2
   // Comment this block if you already have funds on zkSync.
@@ -33,10 +33,10 @@ export default async function (hre: HardhatRuntimeEnvironment) {
   const parsedFee = ethers.utils.formatEther(deploymentFee.toString());
   console.log(`The deployment is estimated to cost ${parsedFee} ETH`);
 
-  const greeterContract = await deployer.deploy(artifact, [greeting]);
+  const greeterContract = await deployer.deploy(artifact, []);
 
   //obtain the Constructor Arguments
-  console.log("constructor args:" + greeterContract.interface.encodeDeploy([greeting]));
+  console.log("constructor args:" + greeterContract.interface.encodeDeploy([]));
 
   // Show the contract info.
   const contractAddress = greeterContract.address;
